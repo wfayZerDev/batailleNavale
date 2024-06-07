89e687149cbb91c5684907c420274ddd0e5a4d55
FayZerDv <40219975+wfayZerDev@users.noreply.github.com>
2024-06-07 11:17:56 +0200

    [CREATION] Identification des btn pour savoir quel btn est préssé ou non

--- batailleNavale/ViewController.swift
+++ batailleNavale/ViewController.swift
@@ -44,8 +44,65 @@ class ViewController: UIViewController {
                    p2_1, p2_2, p2_3, p2_4, p2_5,
                    p3_1, p3_2, p3_3, p3_4, p3_5,
                    p4_1, p4_2, p4_3, p4_4, p4_5]
+    
+        // Définir les tags
+        for (index, button) in buttons.enumerated() {
+            button.tag = index + 1 // Les tags commencent à 1
+        }
+
+        
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+            // Identifiez quel bouton a été pressé en utilisant son tag
+            let tag = sender.tag
+            print("Button with tag \(tag) pressed")
+            
+            // Vous pouvez utiliser le tag pour effectuer des actions spécifiques
+            switch tag {
+            case 1:
+                print("p1_1 action")
+            case 2:
+                print("p1_2 action")
+            case 3:
+                print("p1_3 action")
+            case 4:
+                print("p1_4 action")
+            case 5:
+                print("p1_5 action")
+            case 6:
+                print("p2_1 action")
+            case 7:
+                print("p2_2 action")
+            case 8:
+                print("p2_3 action")
+            case 9:
+                print("p2_4 action")
+            case 10:
+                print("p2_5 action")
+            case 11:
+                print("p3_1 action")
+            case 12:
+                print("p3_2 action")
+            case 13:
+                print("p3_3 action")
+            case 14:
+                print("p3_4 action")
+            case 15:
+                print("p3_5 action")
+            case 16:
+                print("p4_1 action")
+            case 17:
+                print("p4_2 action")
+            case 18:
+                print("p4_3 action")
+            case 19:
+                print("p4_4 action")
+            case 20:
+                print("p4_5 action")
+            default:
+                break
+            }
+        }
 }
 
